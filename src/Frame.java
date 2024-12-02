b4dbeb99484d90aee4738cdebfd471301bcecd3b
defneeyilmaz <defneyilmaz210@gmail.com>
2024-12-02 23:00:14 +0300

    Left panel has been created

--- src/Frame.java
+++ src/Frame.java
@@ -21,6 +21,7 @@ public class Frame extends JFrame {
         JPanel courseListPanel = new JPanel();
         DefaultListModel<String> listModel = new DefaultListModel<>();
         JList<String> list = new JList<>(listModel);
+        public String course;
         JScrollPane scrollPane = new JScrollPane(list);
         JPanel buttonsPanel = new JPanel();
         JButton addButton = new JButton("Add Course");
@@ -35,20 +36,22 @@ public class Frame extends JFrame {
             userPanel.add(userNamePanel);userPanel.add(department);
             //userPanel.setLayout(new BoxLayout(userPanel, BoxLayout.Y_AXIS));
             userPanel.setLayout(new GridLayout(2,0));
-            userPanel.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY, 2));
+            userPanel.setBorder(BorderFactory.createTitledBorder(""));
 
             buttonsPanel.setPreferredSize(new Dimension(width/8, height/16));
             buttonsPanel.add(addButton);
             buttonsPanel.add(removeButton);
-            buttonsPanel.setLayout(new GridLayout(0,2));
-            buttonsPanel.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY, 2));
+            buttonsPanel.setLayout(new GridLayout(0,1));
+            buttonsPanel.setBorder(BorderFactory.createTitledBorder(""));
             //temporary array, like get(department.courses)
             String[] SECourses = new String[]{"SE323", "SE321", "SE311", "SE375", "SE216"};
 
             list.addListSelectionListener(new ListSelectionListener() {
                 @Override
                 public void valueChanged(ListSelectionEvent event) {
-                    //showing course information
+                    course = list.getSelectedValue();
+                    rightPanel.setCourseName(course);
+                    rightPanel.setVisible(true);
                 }
             });
 
@@ -82,6 +85,7 @@ public class Frame extends JFrame {
             list.setVisibleRowCount(5);
 
             scrollPane.setPreferredSize(new Dimension(width/8, (height/16*8)));
+            scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
             courseListPanel.add(scrollPane);
             courseListPanel.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY, 2));
 
@@ -89,26 +93,99 @@ public class Frame extends JFrame {
             setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
         }
     }
-    //------------------Course Panel---------------------
-    CoursePanel coursePanel ;
-    private class CoursePanel extends JPanel{
-        JPanel infoPanel = new JPanel();
-        JLabel label = new JLabel("Course Page");
+    //------------------Right Panel---------------------
+    RightPanel rightPanel ;
+    private class RightPanel extends JPanel {
+        //------------------Course Name Panel---------------------
+        void setCourseName(String courseName){
+            //courseNamePanel = new CourseNamePanel(courseName);
+        }
+        private class CourseNamePanel extends JPanel {
+            JLabel courseNameLabel = new JLabel("Course Name: ");
 
-        public CoursePanel(){
-            add(infoPanel);
-            infoPanel.add(label);
-            setVisible(true);
+            public CourseNamePanel(/*String courseName*/){
+                //courseNameLabel = new JLabel(courseNameLabel.getText() + courseName);
+                add(courseNameLabel);
+            }
+        }
+        //------------------Course Information Panel---------------------
+        private class CourseInfoPanel extends JPanel {
+            JPanel loPanel = new JPanel();
+            JPanel studentPanel = new JPanel();
+            JPanel questionPanel = new JPanel();
+            JPanel examPanel = new JPanel();
+
+            public CourseInfoPanel() {
+                setLayout(new BorderLayout());
+
+                loPanel.setLayout(new BoxLayout(loPanel, BoxLayout.Y_AXIS));
+                loPanel.setBorder(BorderFactory.createTitledBorder("Learning Outcomes"));
+                loPanel.add(new JLabel("LO1"));
+                loPanel.add(new JLabel("LO2"));
+
+                loPanel.setPreferredSize(new Dimension(getWidth(), 310));
+                add(loPanel, BorderLayout.NORTH);
+
+                studentPanel.setLayout(new BoxLayout(studentPanel, BoxLayout.Y_AXIS));
+                studentPanel.setBorder(BorderFactory.createTitledBorder("Students"));
+                studentPanel.add(new JLabel("Section 1: Hüs"));
+                studentPanel.add(new JLabel("Section 2: Def"));
+
+                studentPanel.setPreferredSize(new Dimension(300, getHeight()));
+                add(studentPanel, BorderLayout.EAST);
+
+                questionPanel.setLayout(new BoxLayout(questionPanel, BoxLayout.Y_AXIS));
+                questionPanel.setBorder(BorderFactory.createTitledBorder("Questions"));
+                questionPanel.add(new JLabel("Q1"));
+                questionPanel.add(new JLabel("Q2"));
+
+                examPanel.setLayout(new BoxLayout(examPanel, BoxLayout.Y_AXIS));
+                examPanel.setBorder(BorderFactory.createTitledBorder("Exams"));
+                examPanel.add(new JLabel("Exam 1"));
+                examPanel.add(new JLabel("Exam 2"));
+
+                JPanel leftColumn = new JPanel();
+                leftColumn.setLayout(new GridBagLayout());
+                GridBagConstraints gbc = new GridBagConstraints();
+                gbc.fill = GridBagConstraints.BOTH;
+
+                gbc.weightx = 1.0;
+                gbc.weighty = 0.5;
+                gbc.gridx = 0;
+                gbc.gridy = 0;
+                leftColumn.add(questionPanel, gbc);
+
+                gbc.weightx = 1.0;
+                gbc.weighty = 0.5;
+                gbc.gridx = 0;
+                gbc.gridy = 1;
+                leftColumn.add(examPanel, gbc);
+
+                //they're sharing the remaining part 50-50
+
+                add(leftColumn, BorderLayout.CENTER);
+            }
+        }
+
+        public RightPanel(){
+            setLayout(new BorderLayout());
+            JLabel courseNameLabel = new JLabel();
+            JPanel courseNamePanel = new CourseNamePanel();
+            courseNamePanel.add(courseNameLabel);
+            JPanel courseInfoPanel = new CourseInfoPanel();
+            courseNamePanel.setPreferredSize(new Dimension(getWidth(),25));
+            add(courseNamePanel, BorderLayout.NORTH);
+            courseInfoPanel.setPreferredSize(new Dimension(getWidth(),690));
+            add(courseInfoPanel, BorderLayout.CENTER);
         }
     }
 
-
     public Frame(){
         leftPanel = new LeftPanel();
-        coursePanel = new CoursePanel();
+        rightPanel = new RightPanel();
         add(leftPanel, BorderLayout.WEST);
-        add(coursePanel, BorderLayout.CENTER);
-
+        add(rightPanel, BorderLayout.CENTER);
+        rightPanel.setVisible(false);
 
         setSize(width/4*3, height/4*3);
         setLocation(width/8,height/8);
